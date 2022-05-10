f786b7352e35c96eab1910ab9eba97d2213f34b7
mehmetyorulmaz1080 <100291023+mehmetyorulmaz1080@users.noreply.github.com>
2022-05-11 00:55:07 +0300

    MEHMET YORULMAZ 21640310015
    MEHMET YORULMAZ
    21640310015
    HAFTA 8
    
    Co-Authored-By: evrimguler <26477098+evrimguler@users.noreply.github.com>
    Co-Authored-By: fayaz44 <91744361+fayaz44@users.noreply.github.com>
    Co-Authored-By: bbykptpt <101250113+bbykptpt@users.noreply.github.com>

---
+++ Account.java
@@ -0,0 +1,14 @@
+package hafta8;
+public class Account {
+	String name;
+	int money;
+
+	public Account(String name, int money) {
+		this.name = name;
+		this.money = money;
+	}
+
+	public String toString() {
+		return name + ":$" + money;
+	}
+}
---
+++ List.java
@@ -0,0 +1,17 @@
+package hafta8;
+
+import java.util.ArrayList;
+
+public class List<T> {
+
+	public ArrayList get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void add(ArrayList<Integer> arrayList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
