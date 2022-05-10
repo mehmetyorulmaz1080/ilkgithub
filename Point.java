8d420382c2bc766aa52896e209ac4bf408b45bb9
mehmetyorulmaz1080 <100291023+mehmetyorulmaz1080@users.noreply.github.com>
2022-05-10 16:20:48 +0300

    MEHMET YORUMAZ 21640310015
    MEHMET YORULMAZ
    21640310015
    
    Co-Authored-By: evrimguler <26477098+evrimguler@users.noreply.github.com>
    Co-Authored-By: fayaz44 <91744361+fayaz44@users.noreply.github.com>

--- Point.java
+++ Point.java
@@ -1,6 +1,33 @@
-
 public class Point {
-	
-	int x;
-	int y;
+     private int x;
+     private int y;
+     
+     
+     public Point(int initialX,int initialY) {
+    	 setLocation(initialX, initialY);
+     }
+     public int getX() {
+    	 return x;
+     }
+     public int getY() {
+    	 return y;
+     }
+     
+     
+     
+     public double distanceFromOrigin() {
+    	 return Math.sqrt(x*x+y*y);
+     }
+     
+     public void translate(int dx,int dy) {
+    	 setLocation(x+dx,y+dy);
+     }
+     public void setLocation(int newX,int newY) {
+    	 x=newX;
+    	 y=newY;
+     }
+     public String toString() {
+    	 return "("+x+","+y+")";
+     }
+     
 }
